3ffbff176c6ff8f99742814450bb0b64bed70de5
Ashley-Blu <matsekolengashley28@hmail.com>
2025-10-17 11:05:15 +0200

    Made a connection between thw HTTP requests and controllers

---
+++ src/routes/List.ts
@@ -0,0 +1,71 @@
+import http, { IncomingMessage, ServerResponse } from "http";
+import { addItem, getItemById, getItems } from "../controllers/List";
+import { error } from "console";
+
+export const listsRoute = async (req: IncomingMessage, res: ServerResponse) => {
+  if (req.url?.startsWith("/lists")) {
+    const parts = req.url.split("/");
+    console.log(parts, "url parts");
+
+    const id = parts[2] ? parseInt(parts[2]) : undefined;
+
+    if (req.method === "GET" && !id) {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify(getItems()));
+      return;
+    }
+
+    if (req.method === "GET" && id) {
+      if (isNaN(id)) {
+        res.writeHead(400, { "content-type": "application/json" });
+        res.end(JSON.stringify({ error: "Invalid item id" }));
+        return;
+      }
+      const item = getItemById(id);
+      if (!item) {
+        res.writeHead(404, { "content-type": "application/json" });
+        res.end(JSON.stringify({ error: "Item not found" }));
+        return;
+      }
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify(item));
+      return;
+    }
+
+    if ((req.method = "POST")) {
+      let body = "";
+      req.on("data", (chunk) => {
+        console.log(chunk, "chunk");
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        try {
+          const { name, quantity, status } = JSON.parse(body);
+          if (!name || typeof name !== "string") {
+            res.writeHead(400, { "content-type": "application/json" });
+            res.end(JSON.stringify({ error: "Item name is required" }));
+          }
+          if (!quantity || typeof quantity !== "string") {
+            res.writeHead(400, { "content-type": "application/json" });
+            res.end(JSON.stringify({ error: "Item quantity is required" }));
+          }
+          if (!status || typeof status !== "string") {
+            res.writeHead(400, { "content-type": "application/json" });
+            res.end(JSON.stringify({ error: "Item status is required" }));
+          }
+
+          const newItem = addItem(name, quantity, status);
+          res.writeHead(201, { "content-type": "application/json" });
+          res.end(JSON.stringify(newItem));
+        } catch (error) {
+          res.writeHead(400, { "content-type": "application/json" });
+          res.end(JSON.stringify({ error: "Invalid JSON payload" }));
+        }
+      });
+      return;
+    }
+    res.writeHead(405, { "content-type": "application/json" });
+    res.end(JSON.stringify({ error: "Method not allowed on /lists" }));
+  }
+};
