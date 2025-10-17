797e8309c41ddcb9a750b7e9508e2c4ac92f7eae
Ashley-Blu <matsekolengashley28@hmail.com>
2025-10-17 10:32:17 +0200

    Created the manager for the shopping list - CR of the CRUD operators

---
+++ src/controllers/List.ts
@@ -0,0 +1,44 @@
+import {  List } from '../models/Items';
+
+let items: List[] = [];
+let currentId = 1;
+
+export const getItems = (): List[] => {
+    return items;
+};
+
+export const getItemById = (id: number): List | undefined => {
+    const item = items.find((item) => item.id === id)
+    return item;
+}
+
+export const addItem = (name: string, quantity: string, status: boolean) : List => {
+    const newItem: List = {id: currentId++, name, quantity, status}
+    items.push(newItem)
+    return newItem;
+}
+
+// export const addItem = (name: string, quantity: string, status: ItemStatus = ItemStatus.pending): List => {
+//     const newItem: List = {id: currentId++, name, quantity, status}
+//     items.push(newItem)
+//     return newItem;
+// }
+
+// export const updateItem = (id: number, name: string, quantity: string, status: ItemStatus): List | undefined => {
+//     const item = items.find((item) => item.id === id);
+//     if(item){
+//         item.name = name;
+//         item.quantity = quantity;
+//         item.status = status;
+//     }
+//     return item;
+// }
+
+// export const deleteItem = (id: number): boolean => {
+//     const index = items.findIndex((item) => item.id === id);
+//     if(index !== -1) {
+//         items.splice(index, 1);
+//         return true;
+//     }
+//     return false;
+// }
