021740c3544eefa4e0bfe5fbaefd7310fc5201cf
Ashley-Blu <matsekolengashley28@hmail.com>
2025-10-17 08:42:44 +0200

    Initial server.ts setup with HTTP server

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import http, { IncomingMessage, ServerResponse } from 'http';
+import { listsRoute } from './routes/List'
+
+const PORT = 3000;
+
+const requestListener = (req: IncomingMessage, res: ServerResponse) => {
+    if(req.url?.startsWith('/lists')) {
+        listsRoute(req, res);
+    }else{
+        res.writeHead(200, {"content-type": "applicatio/json"});
+        res.end(JSON.stringify({message: "Hello world!"}));
+    }
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
